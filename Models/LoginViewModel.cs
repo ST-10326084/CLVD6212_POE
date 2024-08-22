6aeb5ce6f55546ad29eb80079be42b56a8a2cc79
Dean Gibson <deangibson03@gmail.com>
2024-08-22 19:47:03 +0200

    Product and PrevOrders working

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class LoginViewModel
+namespace CLVD6212_POE.Models
 {
-    [Required(ErrorMessage = "Username is required.")]
-    public string Username { get; set; }
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
 
-    [Required(ErrorMessage = "Password is required.")]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
 }
+
---
+++ Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace CLVD6212_POE.Models
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+    }
+}
